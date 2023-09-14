32deebe0d47983901ef508ace068f5eb34ef3a5f
qayyum <qerywan@gmail.com>
2023-09-14 17:53:56 +0800

    Added Username validation

--- expenseweb/authentication/urls.py
+++ expenseweb/authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UsernameValidationView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
 ]
